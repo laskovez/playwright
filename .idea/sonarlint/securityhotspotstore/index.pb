77be047a8171a60229603b7e92776359ef99151b
serhiilaskovez <Serhii.Liaskovets.-ND@disney.com>
2024-06-26 17:49:11 +0300

    Перший коміт

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,5 @@
+
+q
+A.venv/lib/python3.9/site-packages/playwright/sync_api/__init__.py,d/7/d71eba3c092871753ac4c242a8b13002eb3026d7
+?
+test_example.py,7/1/71a199f9a5d3fc3a9cab089e5b3ce00d2dafdf3c
---
+++ test_example.py
@@ -0,0 +1,17 @@
+import re
+from playwright.sync_api import Page, expect
+
+def navigate_to_playwright(page: Page):
+    page.goto("https://playwright.dev/")
+
+def test_has_title(page: Page):
+    navigate_to_playwright(page)
+    expect(page).to_have_title(re.compile("Playwright"))
+
+def click_get_started(page: Page):
+    page.get_by_role("link", name="Get started").click()
+
+def test_get_started_link(page: Page):
+    navigate_to_playwright(page)
+    click_get_started(page)
+    expect(page.get_by_role("heading", name="Installation")).to_be_visible()
